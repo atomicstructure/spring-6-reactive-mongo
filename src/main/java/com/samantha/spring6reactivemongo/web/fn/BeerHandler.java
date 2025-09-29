2e7d9b4c7a7e3980dcb805b1d8d3b8185fcf96ed
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-09-29 08:45:38 +0100

    WebFluxfn Router

--- src/main/java/com/samantha/spring6reactivemongo/web/fn/BeerHandler.java
+++ src/main/java/com/samantha/spring6reactivemongo/web/fn/BeerHandler.java
@@ -5,6 +5,7 @@ import com.samantha.spring6reactivemongo.model.BeerDTO;
 import com.samantha.spring6reactivemongo.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
@@ -13,7 +14,7 @@ import reactor.core.publisher.Mono;
 public class BeerHandler {
     private final BeerService beerService;
 
-    public Mono<ServerResponse> listBeers(ServerResponse serverResponse) {
+    public Mono<ServerResponse> listBeers(ServerRequest serverRequest) {
         return ServerResponse.ok()
                 .body(beerService.listBeers(), BeerDTO.class);
     }
