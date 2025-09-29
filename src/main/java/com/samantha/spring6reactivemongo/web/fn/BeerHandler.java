9213513e9bb8ca221aaf5ca23b68a66320b5c848
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-09-29 09:27:28 +0100

    WebFluxfn Router and Handler for Create New Beer Endpoint

--- src/main/java/com/samantha/spring6reactivemongo/web/fn/BeerHandler.java
+++ src/main/java/com/samantha/spring6reactivemongo/web/fn/BeerHandler.java
@@ -8,15 +8,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @Component
 @RequiredArgsConstructor
 public class BeerHandler {
     private final BeerService beerService;
 
+    public Mono<ServerResponse> createBeer(ServerRequest serverRequest) {
+        return beerService.saveBeer(serverRequest.bodyToMono(BeerDTO.class))
+                .flatMap(beerDTO -> ServerResponse
+                        .created(UriComponentsBuilder
+                                .fromPath(BeerRouterConfig.BEER_PATH_ID)
+                                .build(beerDTO.getId()))
+                        .build());
+    }
     public Mono<ServerResponse> getBeerById(ServerRequest serverRequest) {
-        return ServerResponse.ok().body(beerService.getById(serverRequest.pathVariable("beerId")), BeerDTO.class);
+        return ServerResponse.ok()
+                .body(beerService.getById(serverRequest.pathVariable("beerId")), BeerDTO.class);
     }
     public Mono<ServerResponse> listBeers(ServerRequest serverRequest) {
         return ServerResponse.ok()
